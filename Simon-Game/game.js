8e303f3936d89686f9f83c1e5fdb0d5d98926781
mouadia <ouadia.mohammed@outlook.de>
2023-11-20 22:34:23 +0100

    update

---
+++ Simon-Game/game.js
@@ -0,0 +1,12 @@
+
+var gamePattern = [];
+
+var buttonColours =["red", "blue", "green", "yellow"];
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+}
+

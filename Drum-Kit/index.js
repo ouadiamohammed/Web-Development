009840c0b1d41aa3f134080e392249c461b57e89
mouadia <ouadia.mohammed@outlook.de>
2023-11-20 18:09:17 +0100

    update

--- Drum-Kit/index.js
+++ Drum-Kit/index.js
@@ -1,7 +1,43 @@
 
-for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+for (let i = 0; i <numberOfDrumButtons; i++) {
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        alert("i got clicked!");
+        var buttonInnerHTML = this.innerHTML;
+
+        switch (buttonInnerHTML) {
+            case "w":
+                var audio = new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+            
+            case "a":
+                var audio = new Audio("sounds/tom-2.mp3");
+                audio.play();
+                break;
+            case "s":
+                var audio = new Audio("sounds/tom-3.mp3");
+                audio.play();
+                break;
+            
+            case "d":
+                var audio = new Audio("sounds/tom-4.mp3");
+                audio.play();
+                break;
+            case "l":
+                var audio = new Audio("sounds/snare.mp3");
+                audio.play();
+                break;
+            
+            case "j":
+                var audio = new Audio("sounds/crash.mp3");
+                audio.play();
+                break;
+            case "k":
+                var audio = new Audio("sounds/kick-bass.mp3");
+                audio.play();
+                break;
+            default: console.log();
+        }
     });
-} 
+}

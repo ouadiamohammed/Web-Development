b569d3f987d1acacfe41c5709a7486cd4f2c9f70
mouadia <ouadia.mohammed@outlook.de>
2023-12-20 17:10:18 +0100

    update

---
+++ 2.3-NPM/index.js
@@ -0,0 +1,11 @@
+
+import generateName from "sillyname";
+import superheroes from "superheroes"
+
+var sillyName = generateName();
+
+
+console.log(`my name is ${sillyName}.`);
+
+superheroes.all;
+console.log(superheroes.random());

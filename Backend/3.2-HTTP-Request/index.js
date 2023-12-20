b8b7c7fbdf85e5d7d54dd487eb462337973efcfe
mouadia <ouadia.mohammed@outlook.de>
2023-12-20 19:00:44 +0100

    update

--- Backend/3.2-HTTP-Request/index.js
+++ Backend/3.2-HTTP-Request/index.js
@@ -2,6 +2,9 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+    res.send("<h1>Hello Word testing the nodemon<h1>");
+});
 app.listen(port, () => {
     console.log(`Server started on port: ${port}`);
 });
